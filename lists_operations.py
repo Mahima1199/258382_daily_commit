d373d98ed44055074f1361c09da17c8f93ff48f5
Mahima Malhotra <mahimamalhotra11997@gmail.com>
2021-04-23 01:08:43 +0530

    list_operations added

---
+++ lists_operations.py
@@ -0,0 +1,8 @@
+words = ["Mahima","Malhotra","Software","Engineer"]
+print("Mahima" in words)
+print("Malhotra" in words)
+print("Software" in words)
+print("Engineer" in words)
+print("a" in words)
+print("z" not in words)
+print(not "z" in words )
