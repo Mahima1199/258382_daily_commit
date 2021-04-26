9daef4d63d731b086322f70995eb7e303bf3cbae
Mahima Malhotra <mahimamalhotra11997@gmail.com>
2021-04-26 20:01:30 +0530

    list while loop added

---
+++ List_functions.py
@@ -0,0 +1,15 @@
+numbers = [1,2,3,4,5]
+print(numbers.index(1))
+print(numbers.index(2))
+print(numbers.index(3))
+print(numbers.index(4))
+print(numbers.index(5))
+print(max(numbers))
+print(min(numbers))
+print(numbers.count(1))
+numbers.remove(5)
+print(numbers)
+numbers.reverse()
+print(numbers)
+
+
