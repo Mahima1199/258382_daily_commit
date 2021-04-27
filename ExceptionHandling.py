facd6eef0fa9ad7b490e5a6f321c25d54f09caa6
Mahima Malhotra <mahimamalhotra11997@gmail.com>
2021-04-27 16:58:16 +0530

    exception_handling

---
+++ ExceptionHandling.py
@@ -0,0 +1,15 @@
+try:
+    num1 = 10
+    num2 = 2
+    print(num1/num2)
+    print("Done Calculation")
+except ZeroDivisionError:
+    print("An error occured ")
+    print("Due to Zero Division")
+
+try:
+  variable = 10
+  print (10 / 2)
+except ZeroDivisionError:
+  print("Error")
+print("Finished")
