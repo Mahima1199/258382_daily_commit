fa03a9466756c43c9eb3387f9e597244ae2c973e
Mahima Malhotra <mahimamalhotra11997@gmail.com>
2021-04-27 19:37:05 +0530

    exception_handling

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -1,3 +1,4 @@
+# Example1
 try:
     num1 = 10
     num2 = 2
@@ -6,10 +7,32 @@ try:
 except ZeroDivisionError:
     print("An error occured ")
     print("Due to Zero Division")
-
+#Example 2
 try:
   variable = 10
   print (10 / 2)
 except ZeroDivisionError:
   print("Error")
 print("Finished")
+
+#Example 3
+try:
+    variable = 10
+    print(variable+"Hello")
+    print(variable/2)
+except ZeroDivisionError:
+    print("Divided by Zero")
+except(ValueError,TypeError):
+    print("Error Occured")
+
+# Example 4
+try:
+    print("Hello")
+    print(1/0)
+except ZeroDivisionError:
+    print("Divided by Zero")
+finally:
+    print("This code will run no matter what")
+    
+
+
