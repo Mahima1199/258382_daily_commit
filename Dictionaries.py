0c5b9550820245544eca1158a814ca3872ef7d06
Mahima Malhotra <mahimamalhotra11997@gmail.com>
2021-04-28 19:09:45 +0530

    Dictionaries code added

---
+++ Dictionaries.py
@@ -0,0 +1,11 @@
+primary = {"red":[255,0,0],"green":[0,255,0],"blue":[0,0,255]}
+print(primary["red"])
+print(primary["blue"])
+
+primes = {1:2,2:3,4:7,7:17}
+print(primes[primes[4]])
+
+nums = {1:"one",2:"two",3:"three"}
+print(1 in nums)
+print(2 in nums)
+print(4 not in nums)
